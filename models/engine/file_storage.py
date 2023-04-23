fde4e0beb8921cd5da8c0e49a79ae8ff4089bb9b
Ukajoe <111028101+Ukajoe@users.noreply.github.com>
2023-04-23 01:50:06 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,59 +1,68 @@
 #!/usr/bin/python3
-"""
-This module contains the FileStorage class.
-"""
+"""Defines the FileStorage class."""
 import json
-import os
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
+from models.city import City
 from models.place import Place
 from models.review import Review
+from models.state import State
+from models.user import User
 
 
-validClasses = {'BaseModel': BaseModel,
-                'User': User,
-                'State': State,
-                'City': City,
-                'Amenity': Amenity,
-                'Place': Place,
-                'Review': Review}
-
-
-class FileStorage():
-    """Class used for file storage actions."""
+class FileStorage:
+    """Represent an abstracted storage engine.
+    Attributes:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    """
 
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
-        """A method that returns the dictionary __objects."""
-        return(self.__objects)
+    def all(self, cls=None):
+        """Return a dictionary of instantiated objects in __objects.
+        If a cls is specified, returns a dictionary of objects of that type.
+        Otherwise, returns the __objects dictionary.
+        """
+        if cls is not None:
+            if type(cls) == str:
+                cls = eval(cls)
+            cls_dict = {}
+            for k, v in self.__objects.items():
+                if type(v) == cls:
+                    cls_dict[k] = v
+            return cls_dict
+        return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id"""
-        obj_key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[obj_key] = obj
+        """Set in __objects obj with key <obj_class_name>.id."""
+        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
 
     def save(self):
-        """A method that serializes __objects to the JSON file."""
-        with open(self.__file_path, 'w', encoding="utf-8") as fp:
-            jdict_ = {}
-            for k, v in self.__objects.items():
-                dict_ = self.__objects[k].to_dict()
-                jdict_[k] = dict_
-            fp.write(json.dumps(jdict_))
+        """Serialize __objects to the JSON file __file_path."""
+        odict = {o: self.__objects[o].to_dict() for o in self.__objects.keys()}
+        with open(self.__file_path, "w", encoding="utf-8") as f:
+            json.dump(odict, f)
 
     def reload(self):
-        """A method that deserializes the JSON file to __objects."""
-        dict_ = {}
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path, 'r', encoding="utf-8") as fp:
-                str_ = fp.read()
-                dict_ = json.loads(str_)
-                for k, v in dict_.items():
-                    class_ = v['__class__']
-                    create_class = validClasses[class_]
-                    self.__objects[k] = create_class(**v)
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        try:
+            with open(self.__file_path, "r", encoding="utf-8") as f:
+                for o in json.load(f).values():
+                    name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(name)(**o))
+        except FileNotFoundError:
+            pass
+
+    def delete(self, obj=None):
+        """Delete a given object from __objects, if it exists."""
+        try:
+            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
+        except (AttributeError, KeyError):
+            pass
+
+    def close(self):
+        """Call the reload method."""
+        self.reload()
